fe94c9e802d7523ccf06b30ea98a3d980f544ec6
takuru <takuru@xiaohongshu.com>
2023-10-22 15:23:34 +0800

    add

--- Java/com/takuru/leetcode/Main_1402.java
+++ Java/com/takuru/leetcode/Main_1402.java
@@ -3,6 +3,10 @@ package com.takuru.leetcode;
 import java.util.Arrays;
 
 /**
+ * 贪心算法：
+ * 先对satisfaction数组从大到小排序，从满意度最高的开始
+ * 一个结论：排序后遍历满意度时，只有当前满意度和之前满意度的总和大于0，才是正收益
+ *
  * @author takuru@xiaohongshu.com
  * @date 2023/10/22 12:03
  */
@@ -19,6 +23,7 @@ public class Main_1402 {
 class Solution_1402 {
     public int maxSatisfaction(int[] satisfaction) {
         Arrays.sort(satisfaction);
+        // 当前已遍历的满意度时间系数综合
         int preSum = 0;
         int result = 0;
         for (int i = satisfaction.length - 1; i >= 0; i--) {
